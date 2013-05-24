046f073b3aa383b9df66f2777cd2fee717438ebb
Iver Ottesen <iver.ottesen@gmail.com>
2013-05-24 02:22:53 +0200

    Static pages update

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe 'Home page' do
+    it "should have the h1 'Ivero'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Ivero')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => 'Home')
+    end
+  end
+
+  describe 'Help page' do
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => 'Help')
+    end
+  end
+
+  describe 'About page' do
+    it "should have the h1 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => 'About')
+    end
+  end
+
+  describe 'Contact page' do
+    it "should have the h1 'contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => 'Contact')
+    end
+  end
+
+end
